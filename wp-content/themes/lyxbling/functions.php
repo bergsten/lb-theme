2596cbe468540ddf80b7578d4fc3dbfbf393f101
mats.bergsten <mats.bergsten@Mats-PC.occidentdue.se>
2013-10-16 14:15:32 +0200

    Added some microtags to logotype.

--- wp-content/themes/lyxbling/functions.php
+++ wp-content/themes/lyxbling/functions.php
@@ -14,6 +14,43 @@ function woo_custom_breadcrumbs () {
 add_action('woo_loop_before', 'woo_custom_breadcrumbs', 10 );
 
 /*-----------------------------------------------------------------------------------*/
+/* Optionally load custom logo with microtags. */
+/*-----------------------------------------------------------------------------------*/
+function lb_woo_logo () {
+	$settings = woo_get_dynamic_values( array( 'logo' => '' ) );
+	// Setup the tag to be used for the header area (`h1` on the front page and `span` on all others).
+	$heading_tag = 'span';
+	if ( is_home() || is_front_page() ) { $heading_tag = 'h1'; }
+
+	// Get our website's name, description and URL. We use them several times below so lets get them once.
+	$site_title = get_bloginfo( 'name' );
+	$site_url = home_url( '/' );
+	$site_description = get_bloginfo( 'description' );
+?>
+<div id="logo" itemscope itemtype="http://schema.org/Organization">
+<?php
+	// Website heading/logo and description text.
+	if ( ( '' != $settings['logo'] ) ) {
+		$logo_url = $settings['logo'];
+		if ( is_ssl() ) $logo_url = str_replace( 'http://', 'https://', $logo_url );
+
+		echo '<a href="' . esc_url( $site_url ) . '" title="' . esc_attr( $site_description ) . '"><img itemprop="logo" src="' . esc_url( $logo_url ) . '" alt="' . esc_attr( $site_title ) . '" /></a>' . "\n";
+	} // End IF Statement
+
+	echo '<' . $heading_tag . ' class="site-title"><a itemprop="url" href="' . esc_url( $site_url ) . '"><span itemprop="brand">' . $site_title . '</span></a></' . $heading_tag . '>' . "\n";
+	if ( $site_description ) { echo '<span itemprop="description" class="site-description">' . $site_description . '</span>' . "\n"; }
+?>
+</div>
+<?php
+} // End woo_logo()
+// Remove logo code from Canvas - See https://gist.github.com/srikat/5581777
+add_action('wp_head', 'remove_woo_logo');
+function remove_woo_logo() {
+    remove_action('woo_header_inside','woo_logo');
+}
+add_action( 'woo_header_inside', 'lb_woo_logo', 10 );
+
+/*-----------------------------------------------------------------------------------*/
 /* Single Post Author. From https://managewp.com/create-an-authority-site-part-2 */
 /*-----------------------------------------------------------------------------------*/
 if ( ! function_exists( 'woo_author_box' ) ) {
