14fcc2c22cb548dc09c1c1a6cdf9c6ea0761360b
Mats <Mats@PC>
2014-01-28 10:10:05 +0100

    Fixed the thumbnail hover/click thing on front page.

--- wp-content/themes/lyxbling/front-page.php
+++ wp-content/themes/lyxbling/front-page.php
@@ -42,35 +42,38 @@ get_header();
 <div id="3-col-section-promotion" style="text-align: center;">
 <ul id="Grid" style="align: middle;">
 	<li class="mix mix_all" style="display: inline-block; opacity: 1;" data-name="Smyckesguider">
-<div class="view view-tenth hover_effect">
+            <div class="view view-tenth lb-item hover_effect">
 
-<img class="woo-image" alt="Smyckesguider" src="http://lyxbling.se/wp-content/uploads/2013/09/diamanter-300x300.jpg" width="300" height="300" /> <span class="img-title">Smyckesguider</span>
-<div class="mask">
-<h2><a href="http://lyxbling.se/smyckesguider">Smyckesguider</a></h2>
-Lär dig mer om diamanter, ädelstenar, pärlor, ädelmetaller som guld, roséguld, platina, silver, stål, m.m. i vår smyckesguide...<br />
-<div class="lb-button small" style="clear: both;" data-url="http://lyxbling.se/smyckesguider"><a href="http://lyxbling.se/smyckesguider">Läs mer »</a></div>
-</div>
-</div></li>
+                <img class="woo-image hover_effect" alt="Smyckesguider" src="http://lyxbling.se/wp-content/uploads/2013/09/diamanter-300x300.jpg" width="300" height="300" /> <span class="img-title">Smyckesguider</span>
+                <div class="mask hover_effect">
+                    <h2 class="hover_effect"><a href="http://lyxbling.se/smyckesguider">Smyckesguider</a></h2>
+                    <p class="hover_effect">Lär dig mer om diamanter, ädelstenar, pärlor, ädelmetaller som guld, roséguld, platina, silver, stål, m.m. i vår smyckesguide...</p>
+                    <div class="lb-button small" style="clear: both;" data-url="http://lyxbling.se/smyckesguider"><a href="http://lyxbling.se/smyckesguider">Läs mer »</a></div>
+                </div>
+            </div>
+        </li>
 	<li class="mix mix_all" style="display: inline-block; opacity: 1;" data-name="Smyckesvarumärken">
-<div class="view view-tenth hover_effect">
+            <div class="view view-tenth lb-item hover_effect">
 
-<img class="woo-image" alt="Smyckesvarumärken" src="http://lyxbling.se/wp-content/uploads/2013/08/mercedes-gp-petronas-thomas-sabo-300x200.jpg" width="300" height="300" /> <span class="img-title">Smyckesvarumärken</span>
-<div class="mask">
-<h2><a href="http://lyxbling.se/smyckesvarumarken">Smyckesvarumärken</a></h2>
-Läs om olika kända &amp; okända smyckesvarumärken och lär dig vilka designer som kännetecknar de olika smyckesvarumärkena...<br />
-<div class="lb-button small" style="clear: both;" data-url="http://lyxbling.se/smyckesvarumarken"><a href="http://lyxbling.se/smyckesvarumarken">Läs mer »</a></div>
-</div>
-</div></li>
+                <img class="woo-image hover_effect" alt="Smyckesvarumärken" src="http://lyxbling.se/wp-content/uploads/2013/08/mercedes-gp-petronas-thomas-sabo-300x200.jpg" width="300" height="300" /> <span class="img-title">Smyckesvarumärken</span>
+                <div class="mask hover_effect">
+                    <h2 class="hover_effect"><a href="http://lyxbling.se/smyckesvarumarken">Smyckesvarumärken</a></h2>
+                    <p class="hover_effect">Läs om olika kända &amp; okända smyckesvarumärken och lär dig vilka designer som kännetecknar de olika smyckesvarumärkena...</p>
+                    <div class="lb-button small" style="clear: both;" data-url="http://lyxbling.se/smyckesvarumarken"><a href="http://lyxbling.se/smyckesvarumarken">Läs mer »</a></div>
+                </div>
+            </div>
+        </li>
 	<li class="mix mix_all" style="display: inline-block; opacity: 1;" data-name="Smyckesbutiker">
-<div class="view view-tenth hover_effect">
+            <div class="view view-tenth lb-item hover_effect">
 
-<img class="woo-image" alt="Smyckesbutiker" src="http://lyxbling.se/wp-content/uploads/2014/01/pandorashop-onlinebutik-185x300.png" width="300" height="300" /> <span class="img-title">Smyckesbutiker</span>
-<div class="mask">
-<h2><a href="http://lyxbling.se/smyckesbutiker">Smyckesbutiker</a></h2>
-Vi granskar svenska &amp; utländska smyckesbutiker online - Du väljer var du vill shoppa...<br />
-<div class="lb-button small" style="clear: both;" data-url="http://lyxbling.se/smyckesbutiker"><a href="http://lyxbling.se/smyckesbutiker">Läs mer »</a></div>
-</div>
-</div></li>
+                <img class="woo-image hover_effect" alt="Smyckesbutiker" src="http://lyxbling.se/wp-content/uploads/2014/01/pandorashop-onlinebutik-185x300.png" width="300" height="300" /> <span class="img-title">Smyckesbutiker</span>
+                <div class="mask hover_effect">
+                    <h2 class="hover_effect"><a href="http://lyxbling.se/smyckesbutiker">Smyckesbutiker</a></h2>
+                    <p class="hover_effect">Vi granskar svenska &amp; utländska smyckesbutiker online - Du väljer var du vill shoppa...</p>
+                    <div class="lb-button small" style="clear: both;" data-url="http://lyxbling.se/smyckesbutiker"><a href="http://lyxbling.se/smyckesbutiker">Läs mer »</a></div>
+                </div>
+            </div>
+        </li>
 </ul>
 </div>
             </section><!-- /#main -->
